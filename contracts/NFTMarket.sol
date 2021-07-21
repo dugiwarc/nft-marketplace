827cddef785d59769cbeb1ca8078c6befc408306
dugiwarc <dugiwarc>
2021-07-21 11:08:14 +0200

    add createMarketSale to NFTMarket

--- contracts/NFTMarket.sol
+++ contracts/NFTMarket.sol
@@ -82,4 +82,23 @@ contract NFTMarket is ReentracyGuard {
             false
         )
     }
+
+    function createMarketSale(
+        address nftContract,
+        uint256 itemId
+    ) public payable nonReentrant {
+        uint price = idToMarketItem[itemId].price;
+        uint tokenId = idToMarketItem[itemId].tokenId;
+
+        require(msg.value == price, "Please submit the asking price in order to complete the purchase")
+
+        idToMarketItem[itemId].seller.transfer(msg.value);
+        IERC721(nftContract).transferFrom(address(this), msg.sender, tokenId);
+        idToMarketItem[itemId].owner = payable(msg.sender)
+        idToMarketItem[itemId].sold = true;
+        _itemsSold.increment();
+        payable(owner).transfer(listingPrice)
+    }
+
+    
 }
